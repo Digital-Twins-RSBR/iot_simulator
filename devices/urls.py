76dddb5564e61aa8514cde4b9cda1c705380dca8
Lucas Silva Pereira <lucas.pereira@ifrn.edu.br>
2025-02-03 15:05:06 -0300

    adding index end performing changes

--- devices/urls.py
+++ devices/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Endpoint para que o gateway do ThingsBoard envie comandos RPC para um dispositivo específico.
-    path('rpc/<str:device_id>/', views.rpc_endpoint, name='rpc_endpoint'),
 ]
--- iot_simulator/urls.py
+++ iot_simulator/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from devices.views import index
+
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
      path('devices/', include('devices.urls')),
 ]
